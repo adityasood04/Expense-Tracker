69c8a7b0ff8837ba2a867d8ee1ddb29b50f5bf25
Aditya Sood <adityasood314@gmail.com>
2023-07-03 18:25:17 +0530

    Added bar chart to all categories

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,17 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
-
-
 
 final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
-enum Category{
-  food, travel, leisure, work,
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
 }
-
 
 const categoryIcons = {
   Category.food: Icons.lunch_dining,
@@ -20,14 +22,13 @@ const categoryIcons = {
   Category.work: Icons.work,
 };
 
-class Expense{
-
-  Expense({
-    required this.title, 
-    required this.amount, 
-    required this.date,
-    required this.category
-  }) : id = uuid.v4();
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
@@ -35,10 +36,25 @@ class Expense{
   final DateTime date;
   final Category category;
 
-  String get formattedDate{
+  String get formattedDate {
     return formatter.format(date);
   }
+}
 
- 
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
 
+  final Category category;
+  final List<Expense> expenses;
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum = sum + expense.amount;
+    }
+    return sum;
+  }
 }
